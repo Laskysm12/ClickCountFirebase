f23011eeb7eca34f7d4b7fae6de31dc1bf70548e
Sam Lasky <Laskysm@gmail.com>
2018-03-11 18:22:38 -0400

    another commit

---
+++ javascript/logic.js
@@ -0,0 +1,27 @@
+ var config = {
+  apiKey: "AIzaSyATymnNHhamMAjQ5cbOcQSPFWrEN34HfIQ",
+  authDomain: "clickcount-1171e.firebaseapp.com",
+  databaseURL: "https://clickcount-1171e.firebaseio.com",
+  storageBucket: "clickcount-1171e.appspot.com",
+  };  
+
+  firebase.initializeApp(config);
+
+  var count = 100;
+
+  firebase.database().ref().on("value",function(snapshot) {
+      count = snapshot.val().clicks;
+      console.log(snapshot.val());
+      $('#clickValue').html(count);
+      // Need to say snapshot.val to get what you want returned
+  });
+
+  $('#clickButton').on("click",function(){
+      count--;
+      firebase.database().ref().set({
+          clicks:count
+    })
+
+  })
+
+ 
